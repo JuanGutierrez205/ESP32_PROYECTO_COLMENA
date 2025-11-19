7f14dd8de74a0fa11ed3b562de7799d03d028fa6
chhriie <152648313+chhriie@users.noreply.github.com>
2025-11-18 22:34:41 -0500

    Add files via upload

---
+++ Pagina Web/conexion.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = "####";   // Host MySQL
+$user = "####";              // Usuario MySQL
+$pw = "####";     // La misma contraseña de tu cuenta InfinityFree
+$db = "####";       // Nombre de tu base de datos
+
+?>
