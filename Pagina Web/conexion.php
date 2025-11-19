408c6a638d9b7cf4b1ebac05ab1446b3e6ff670b
chhriie <152648313+chhriie@users.noreply.github.com>
2025-11-18 22:32:38 -0500

    Delete Pagina Web/conexion.php

--- Pagina Web/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$host = "sql110.infinityfree.com";   // Host MySQL
-$user = "if0_40451815";              // Usuario MySQL
-$pw = "Sosourcandy123";     // La misma contraseña de tu cuenta InfinityFree
-$db = "if0_40451815_proy_int";       // Nombre de tu base de datos
-
-?>
